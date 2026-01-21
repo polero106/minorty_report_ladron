f26e94a32f285c2064283219f480385749867ab0
Jose Antonio Castro <joseancascas12@gmail.com>
2026-01-21 19:14:05 +0100

    modified:   caraginicial.py 	new file:   etl_policial.py 	new file:   modelos/modelos.py 	new file:   requirements.txt

--- caraginicial.py
+++ caraginicial.py
@@ -2,33 +2,34 @@ import json
 from neo4j import GraphDatabase
 
 # Configuración (Ajusta según tu entorno local o AuraDB)
-URI = "bolt://localhost:7687"
-AUTH = ("neo4j", "tu_password")
+URI = "neo4j+ssc://5d9c9334.databases.neo4j.io"
+AUTH = ("neo4j", "oTzaPYT99TgH-GM2APk0gcFlf9k16wrTcVOhtfmAyyA")
+
 
 def cargar_datos(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f)
 
     driver = GraphDatabase.driver(URI, auth=AUTH)
-    
+
     query_personas = """
     UNWIND $batch as item
     MERGE (p:Persona {id: item.id})
     SET p.nombre = item.nombre, p.historial = item.historial, p.embedding = [0.1, 0.5] // Dummy embedding
     """
-    
+
     query_ubicaciones = """
     UNWIND $batch as item
     MERGE (u:Ubicacion {id: item.id})
     SET u.distrito = item.distrito, u.peligro = item.peligro
     """
-    
+
     query_crimenes = """
     UNWIND $batch as item
     MERGE (c:Crime {id: item.id})
     SET c.tipo = item.tipo, c.timestamp = item.timestamp
     """
-    
+
     query_relaciones = """
     UNWIND $batch as item
     MATCH (s {id: item.source})
@@ -46,10 +47,11 @@ def cargar_datos(file_path):
         session.run(query_crimenes, batch=data['crimenes_futuros'])
         print("Creando Relaciones...")
         session.run(query_relaciones, batch=data['relaciones'])
-        
+
     driver.close()
     print("¡Datos cargados! La 'Policía' ya tiene un grafo para analizar.")
 
+
 # Ejecutar
 if __name__ == "__main__":
-    cargar_datos('minority_report_data.json')
+    cargar_datos('datos/prueba.json')
